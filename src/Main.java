0495b19862b354ab49c7f2f9e57843f6d5ac4c6e
Tatianna Hines <tchines2028@gmail.com>
2026-08-16 04:37:26 -0500

    Added shared interest matching

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,15 @@ public class Main {
         Student student1 = new Student("Alex", "alex@wisc.edu", "he/him", "Computer Science", 1);
         Student student2 = new Student("Mary", "mary@wisc.edu", "she/her", "Computer Science", 1);
 
-        student1.introduce();
-        student2.introduce();
-
 
         student1.addIdentity("POC");
         student1.addIdentity("2SLGBTQIA+");
         student1.addIdentity("Late-diagnosed");
         student1.listIdentities();
+        student2.addIdentity("POC");
+        System.out.println(student1.getIdentities());
 
+        student1.sharedIdentity(student2);
 
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class Student {
 
     //Student data
-    String name; //First and Last Name of Student
-    String email;
-    String pronouns; //Student's pronouns
-    String major; //Major
-    int year; //Year in college by completion not age
-    HashSet<String> identity = new HashSet<>(); //Marginalized identity groups Student is a part of
+    private String name; //First and Last Name of Student
+    private String email;
+    private String pronouns; //Student's pronouns
+    private String major; //Major
+    private int year; //Year in college by completion not age
+    private HashSet<String> identity = new HashSet<>(); //Marginalized identity groups Student is a part of
     Set<String> possibleIdentities = Set.of("POC", "2SLGBTQIA+", "Non-traditional", "Late-diagnosed", "Undiagnosed", "Non-speaking"); //contains a list of the possible identity groups a student can be a part of
 
 
@@ -22,19 +22,6 @@ public class Student {
 
     }
 
-    public void introduce(){
-        System.out.println("Hello, my name is " + name + " and I am a " + year + "(st) year student studying " + major );
-    }
-
-    public void deleteIdentity(String identityLabel){
-        if(possibleIdentities.contains(identityLabel)) {
-            System.out.println("Deleted " + identity.remove(identityLabel) + " from " + name + "'s list of identities");
-        }
-
-        else {
-            System.out.println("Label does not exist in list");
-        }
-    }
 
     /*
      * Adds an identity label to the Student
@@ -49,6 +36,21 @@ public class Student {
             System.out.println("Identity not possible to add");
         }
     }
+
+    /*
+     * Prints/Lists the identities the current Student holds
+     * returns void
+     * */
+    public void deleteIdentity(String identityLabel){
+        if(possibleIdentities.contains(identityLabel)) {
+            System.out.println("Deleted " + identity.remove(identityLabel) + " from " + name + "'s list of identities");
+        }
+
+        else {
+            System.out.println("Label does not exist in list");
+        }
+    }
+
     /*
      * Prints/Lists the identities the current Student holds
      * returns void
@@ -60,5 +62,26 @@ public class Student {
             System.out.println(label);
         }
     }
+
+    public HashSet<String> getIdentities(){
+        return identity;
+    }
+
+    /* Finds the shared identities between this student and another
+     * Parameters: Student
+     * returns a HashSet of shared identities between the two students
+     * */
+    public HashSet<String> sharedIdentity(Student otherStudent){
+        HashSet<String> shared_identity_list = new HashSet<>();
+        for(String label : identity){
+            if (otherStudent.getIdentities().contains(label)){
+                shared_identity_list.add(label);
+            }
+        }
+
+        System.out.println(shared_identity_list);
+        return shared_identity_list;
+    }
+
 
 }
